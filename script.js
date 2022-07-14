963a2fe187010212f96da68c1f66b327debc678f
Cinzia-I-M <cinzia.imbriano@gmail.com>
2022-07-14 22:26:00 +0100

    Hero header and scrollbars

---
+++ script.js
@@ -0,0 +1,18 @@
+setTimeout(function(){
+    document.getElementById("keyWord").innerHTML = 'book';
+}, 2000);
+setTimeout(function(){
+    document.getElementById("keyWord").innerHTML = 'film';
+}, 4000);
+setTimeout(function(){
+    document.getElementById("keyWord").innerHTML = 'podcast';
+}, 6000);
+setTimeout(function(){
+    document.getElementById("keyWord").innerHTML = 'instagram';
+}, 8000);
+setTimeout(function(){
+    document.getElementById("keyWord").innerHTML = 'TV show';
+}, 10000);
+setTimeout(function(){
+    document.getElementById("keyWord").innerHTML = 'obsession';
+}, 12000);
