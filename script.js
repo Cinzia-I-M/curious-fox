7765c93b56d93276266be23834b5b3a2040eb9d7
Cinzia-I-M <cinzia.imbriano@gmail.com>
2022-07-27 11:39:22 +0100

    Pick for me button javascript

--- script.js
+++ script.js
@@ -100,3 +100,32 @@ email.addEventListener("input", function (event) {
     }
 });
 
+// Pick for me random choice
+
+function PickRandom(){
+    const myrandom=Math.round(Math.random()*8)
+    const link1="https://www.netflix.com/title/81000864"
+    const link2="https://www.amazon.co.uk/Age-AI-Our-Human-Future/dp/1529375975/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=1657824167&sr=8-1"
+    const link3="https://www.amazon.co.uk/Lord-Rings-Fellowship-Alan-Howard/dp/B00IHRUR3I"
+    const link4="https://www.youtube.com/c/ZoeSugg"
+    const link5="https://twitter.com/TheMontyDon?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+    const link6="https://www.skysports.com/womens-football"
+    const link7="https://www.amazon.co.uk/Knitting-Book-Step-Step-Techniques/dp/024136194X/ref=asc_df_024136194X/?tag=googshopuk-21&linkCode=df0&hvadid=375480601620&hvpos=&hvnetw=g&hvrand=8872891340466283660&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1006490&hvtargid=pla-815171494739&psc=1&th=1&psc=1&tag=&ref=&adgrpid=76471992426&hvpone=&hvptwo=&hvadid=375480601620&hvpos=&hvnetw=g&hvrand=8872891340466283660&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1006490&hvtargid=pla-815171494739"
+    const link8="https://www.bbc.co.uk/programmes/m001955r"
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==2)
+        window.location=link3
+    else if (myrandom==3)
+        window.location=link4
+    else if (myrandom==4)
+        window.location=link5
+    else if (myrandom==5)
+        window.location=link6
+    else if (myrandom==6)
+        window.location=link7
+    else if (myrandom==7)
+        window.location=link8
+}
